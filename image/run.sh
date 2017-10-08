b879e39fe449117c1fc72ddea12ed71413b5947b
Changwoo Ryu <cwryu@debian.org>
2017-10-09 02:37:13 +0900

    Correct persistent DB docker volume
    - Use /var/lib/mysql instead of subdir.

--- image/run.sh
+++ image/run.sh
@@ -1,5 +1,15 @@
-if ! [ -f /var/www/html/w/LocalSettings.php ]; then
+# wait for db
+cd /var/www/html/w
+
+while ! mysqladmin ping -h db --silent; do
+    sleep 1
+done
+
+if ! mysqlshow -h db -u root --password=$MEDIAWIKI_DB_PASSWORD galkwikiki >/dev/null; then
+    bash /app/setup.sh
+elif ! [ -e LocalSettings.php ]; then
     bash /app/setup.sh
 fi
+
 . /etc/apache2/envvars
 exec apache2 -DFOREGROUND
--- image/setup.sh
+++ image/setup.sh
@@ -2,27 +2,40 @@ set -e
 
 cd /var/www/html/w
 
-# wait for db
-while ! mysqladmin ping -h db --silent; do
-    sleep 1
-done
-
 LANG=ko_KR.UTF-8
 export LANG
 
-php maintenance/install.php \
-    --dbserver db --dbtype mysql --dbname galkwiki \
-    --dbuser root --dbpass $MEDIAWIKI_DB_PASSWORD \
-    --scriptpath "/w" \
-    --installdbpass $MEDIAWIKI_DB_PASSWORD --installdbuser root \
-    --lang ko --pass $MEDIAWIKI_ADMIN_PASSWORD \
-    "갈퀴" Admin
+if ! mysqlshow -h db -u root --password=$MEDIAWIKI_DB_PASSWORD galkwiki >/dev/null; then
+    INIT_DB=yes
+else
+    INIT_DB=no
+fi
 
-mv LocalSettings.php conf/LocalSettings.php.install
-sed -e s%@MEDIAWIKI_URL@%$MEDIAWIKI_URL%';'s%@MEDIAWIKI_DB_PASSWORD@%$MEDIAWIKI_DB_PASSWORD% \
-    LocalSettings.php.dist > conf/LocalSettings.php
-ln -s conf/LocalSettings.php LocalSettings.php
+if ! [ -e LocalSettings.php ] || ! [ -e conf/LocalSettings.php ]; then
+    INIT_CONF=yes
+else
+    INIT_CONF=no
+fi
 
-php maintenance/update.php
-php extensions/FlaggedRevs/maintenance/updateAutoPromote.php
-php extensions/SemanticMediaWiki/maintenance/SMW_setup.php
+if [ $INIT_DB = yes ]; then
+    php maintenance/install.php \
+        --dbserver db --dbtype mysql --dbname galkwiki \
+        --dbuser root --dbpass $MEDIAWIKI_DB_PASSWORD \
+        --scriptpath "/w" \
+        --installdbpass $MEDIAWIKI_DB_PASSWORD --installdbuser root \
+        --lang ko --pass $MEDIAWIKI_ADMIN_PASSWORD \
+        "갈퀴" Admin
+    mv LocalSettings.php conf/LocalSettings.php.install
+fi
+
+if [ $INIT_CONF = yes ]; then
+    sed -e s%@MEDIAWIKI_URL@%$MEDIAWIKI_URL%';'s%@MEDIAWIKI_DB_PASSWORD@%$MEDIAWIKI_DB_PASSWORD% \
+        LocalSettings.php.dist > conf/LocalSettings.php
+    ln -s conf/LocalSettings.php LocalSettings.php
+fi
+
+if [ $INIT_DB = yes ]; then
+    php maintenance/update.php
+    php extensions/FlaggedRevs/maintenance/updateAutoPromote.php
+    php extensions/SemanticMediaWiki/maintenance/SMW_setup.php
+fi
